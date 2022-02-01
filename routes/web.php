30ca2ad406210db7d08382a32e90ae0956bb0dfb
Osman Moharram <o.moharram91@gmail.com>
2022-02-01 17:42:44 +0300

    fixed cv

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::post('/', [MessageController::class, 'store']);
 
 Route::get('/about', fn() => view('pages.about') );
 
-Route::get('/about/download', fn() => Response::download(public_path('osmanMoharramResume.pdf', 'Osman Resume')));
+Route::get('/about/download', fn() => Response::download(public_path('cv.pdf', 'Osman Resume')));
